b70a035ed70afed0f8d58fbc09418f4fa1d770c4
Joel Cheah <gjoelcheah@gmail.com>
2021-12-19 16:18:33 +0800

    Finished Quiz Module basic logic without other aesthetics

--- app/src/main/java/com/example/nobulijava/activity/TakeQuizActivity.java
+++ app/src/main/java/com/example/nobulijava/activity/TakeQuizActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -38,6 +37,9 @@ public class TakeQuizActivity extends AppCompatActivity implements View.OnClickL
     private DatabaseReference mDatabase;
 
     private ArrayList<QuizObj> quizObjArrayList = new ArrayList<>();
+    private ArrayList<Integer> scoreArrayList = new ArrayList<>();
+    private Integer currentQuestionIndex = 0;
+    private Integer score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -74,7 +76,9 @@ public class TakeQuizActivity extends AppCompatActivity implements View.OnClickL
                 for (DataSnapshot quizSnapshot: dataSnapshot.getChildren()) {
                     QuizObj quizObj = quizSnapshot.getValue(QuizObj.class);
                     quizObjArrayList.add(quizObj);
+                    scoreArrayList.add(0);
                 }
+                textViewQuestion.setText(quizObjArrayList.get(0).getQuestion());
             }
 
             @Override
@@ -87,127 +91,75 @@ public class TakeQuizActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
 
-
             case R.id.button_next:
-                for (QuizObj quiz: quizObjArrayList){
-                    System.out.println(quiz);
+                //Check for answer when click next
+                if (radioTrue.isChecked()) {
+                    checkAnswer(true);
+                } else if (radioFalse.isChecked()) {
+                    checkAnswer(false);
                 }
-//                //Check for answer when click next
-//                if (radioTrue.isChecked()) {
-//                    checkAnswer(true);
-//                } else if (radioFalse.isChecked()) {
-//                    checkAnswer(false);
-//                }
-//
-//                // go to next Question
-//                // limiting Question bank range
-//                if (currentQuestionIndex <= noOfQuestion) {
-//                    currentQuestionIndex
-//                            = currentQuestionIndex + 1;
-//                    // making buttons
-//                    // invisible
-//                    if (currentQuestionIndex == noOfQuestion) {
-//                        textViewQuestion.setText(getString(
-//                                R.string.correct, correct));
-//                        buttonNext.setVisibility(
-//                                View.GONE);
-//                        buttonPrevious.setVisibility(
-//                                View.GONE);
-//                        radioTrue.setVisibility(
-//                                View.GONE);
-//                        radioFalse.setVisibility(
-//                                View.GONE);
-//                        imageQuizImage.setVisibility(View.GONE);
-//                        buttonTryAgain.setVisibility(View.VISIBLE);
-//                        calcScore();
-//                        textViewQuestion.setText("You answered " + correct + " out of 6! \n Here's " + correct + " water droplets for you!");
-//                    } else {
-//                        updateQuestion();
-//                    }
-//                }
-//
-//                break;
+
+                // go to next Question
+                currentQuestionIndex = currentQuestionIndex + 1;
+                updateQuestion(currentQuestionIndex);
+                break;
 
             case R.id.button_previous:
-//                if (currentQuestionIndex > 0) {
-//                    currentQuestionIndex
-//                            = (currentQuestionIndex - 1)
-//                            % questionBank.length;
-//                    updateQuestion();
-//                }
+                currentQuestionIndex = currentQuestionIndex - 1;
+                updateQuestion(currentQuestionIndex);
                 break;
 
             case R.id.button_tryAgain:
-//                Intent intent_current = getIntent();
-//                finish();
-//                startActivity(intent_current);
+                startActivity(new Intent(TakeQuizActivity.this, TakeQuizActivity.class));
+                finish();
                 break;
 
             case R.id.button_mainMenu:
-//                Intent intent_current = getIntent();
-//                finish();
-//                startActivity(intent_current);
+                startActivity(new Intent(TakeQuizActivity.this, UserDashboardActivity.class));
+                finish();
                 break;
         }
     }
 
-    private void updateQuestion() {
-        radioGroupAnswer.clearCheck();
+    private void updateQuestion(Integer currentQuestionIndex) {
 
-//        questionTextView.setText(
-//                questionBank[currentQuestionIndex]
-//                        .getAnswerResId());
-        // setting the textview with new Question
-//        switch (currentQuestionIndex) {
-//            case 0:
-//                // setting up image for each
-//                // Question
-//                imageQuizImage.setImageResource(R.drawable.a);
-//                break;
-//            case 1:
-//                imageQuizImage.setImageResource(R.drawable.b);
-//                break;
-//            case 2:
-//                imageQuizImage.setImageResource(R.drawable.c);
-//                break;
-//            case 3:
-//                imageQuizImage.setImageResource(R.drawable.d);
-//                break;
-//            case 4:
-//                imageQuizImage.setImageResource(R.drawable.e);
-//                break;
-//            case 5:
-//                imageQuizImage.setImageResource(R.drawable.f);
-//                break;
-//        }
+        // making buttons invisible
+        if (currentQuestionIndex < quizObjArrayList.size()) {
+            radioGroupAnswer.clearCheck();
+            textViewQuestion.setText(quizObjArrayList.get(currentQuestionIndex).getQuestion());
+        } else {
+            calcScore();
+            textViewQuestion.setText(getString(R.string.score) + " " + score.toString());
+//            textViewQuestion.setText(score.toString());
+            buttonNext.setVisibility(View.GONE);
+            buttonPrevious.setVisibility(View.GONE);
+            radioTrue.setVisibility(View.GONE);
+            radioFalse.setVisibility(View.GONE);
+            imageQuizImage.setVisibility(View.GONE);
+            buttonTryAgain.setVisibility(View.VISIBLE);
+            buttonMainMenu.setVisibility(View.VISIBLE);
+        }
     }
 
-//    private void checkAnswer(boolean userChooseCorrect) {
-//        boolean answerIsTrue
-//                = questionBank[currentQuestionIndex]
-//                .isAnswerTrue();
-//        // getting correct ans of current Question
-//        int toastMessageId;
-//        // if ans matches with the
-//        // button clicked
-//
-//        if (userChooseCorrect == answerIsTrue) {
-//            toastMessageId = R.string.correct_answer;
-//            answerArray[currentQuestionIndex] = 1;
-//        } else {
-//            // showing toast
-//            // message correct
-//            toastMessageId = R.string.wrong_answer;
-//            answerArray[currentQuestionIndex] = 0;
-//        }
-//
-//        Toast.makeText(Quiz.this, toastMessageId,
-//                Toast.LENGTH_SHORT).show();
-//    }
-//
-//    private void calcScore(){
-//        for (int i = 0; i<noOfQuestion; i++){
-//            correct = correct + answerArray[i];
-//        }
-//    }
+    private void checkAnswer(Boolean userChoice) {
+        Boolean answer = quizObjArrayList.get(currentQuestionIndex).getAnswer();
+        int toastMessageID;
+
+        if (userChoice == answer) {
+            toastMessageID = R.string.correct_answer;
+            scoreArrayList.set(currentQuestionIndex, 1);
+        } else {
+            toastMessageID = R.string.wrong_answer;
+            scoreArrayList.set(currentQuestionIndex, 0);
+        }
+
+        Toast.makeText(TakeQuizActivity.this, toastMessageID,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void calcScore(){
+        for (int i = 0; i < scoreArrayList.size(); i++){
+            score = score + scoreArrayList.get(i);
+        }
+    }
 }
