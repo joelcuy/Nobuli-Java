bace1c4c0117228f3fabca7e3a6e9671080dd232
Joel Cheah <gjoelcheah@gmail.com>
2021-12-19 22:09:47 +0800

    Create activity for read and set news

---
+++ app/src/main/java/com/example/nobulijava/activity/ReadNewsDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.nobulijava.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nobulijava.R;
+
+public class ReadNewsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_news_details);
+    }
+}
---
+++ app/src/main/java/com/example/nobulijava/activity/SetNewsDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.nobulijava.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nobulijava.R;
+
+public class SetNewsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_news_details);
+    }
+}
