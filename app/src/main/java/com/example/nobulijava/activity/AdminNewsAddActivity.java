703852796a7dcea1816af6f647acc8b5fafd1437
Joel Cheah <gjoelcheah@gmail.com>
2021-12-24 21:14:32 +0800

    admin side add news upload image

--- app/src/main/java/com/example/nobulijava/activity/AdminNewsAddActivity.java
+++ app/src/main/java/com/example/nobulijava/activity/AdminNewsAddActivity.java
@@ -1,19 +1,34 @@
 package com.example.nobulijava.activity;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nobulijava.R;
 import com.example.nobulijava.model.NewsObj;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -24,24 +39,24 @@ public class AdminNewsAddActivity extends AppCompatActivity {
     private Button buttonSaveNews;
     private DateTimeFormatter dateTimeFormatter;
 
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    ImageView imageViewNews;
+    ProgressBar progressBar;
+    String FOLDER_NAME = "news_image";
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    Uri uriSelectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_news_add);
+        getSupportActionBar().setTitle("Add News");
+        findView();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        editTextTitle = findViewById(R.id.editText_adminNewsAdd_title);
-        editTextContent = findViewById(R.id.editText_adminNewsAdd_content);
-        textViewDatePosted = findViewById(R.id.textView_adminNewsAdd_datePosted);
-        buttonSaveNews = findViewById(R.id.button_adminNewsAdd_saveNews);
 
         dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime currentDateTime = LocalDateTime.now();
-
-
         textViewDatePosted.setText(dateTimeFormatter.format(currentDateTime));
 
         buttonSaveNews.setOnClickListener(new View.OnClickListener() {
@@ -52,11 +67,75 @@ public class AdminNewsAddActivity extends AppCompatActivity {
                 String datePostedString = textViewDatePosted.getText().toString();
 
                 NewsObj newNews = new NewsObj(titleString, contentString, datePostedString);
-                mDatabase.child("News").push().setValue(newNews);
+                DatabaseReference pushedNewsRef = mDatabase.child("News").push();
+                String newsID = pushedNewsRef.getKey();
+                pushedNewsRef.setValue(newNews).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        putImageInStorage(newsID);
+                    }
+                });
                 finish();
             }
         });
 
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        onImageSelected(uri);
+                    }
+                });
 
+        imageViewNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");
+            }
+        });
+    }
+
+    public void findView(){
+        editTextTitle = findViewById(R.id.editText_adminNewsAdd_title);
+        editTextContent = findViewById(R.id.editText_adminNewsAdd_content);
+        textViewDatePosted = findViewById(R.id.textView_adminNewsAdd_datePosted);
+        buttonSaveNews = findViewById(R.id.button_adminNewsAdd_saveNews);
+        imageViewNews = findViewById(R.id.imageView_adminNewsAdd_imageUpload);
+        progressBar = findViewById(R.id.progressBar_adminNewsAdd_uploadProgress);
+    }
+
+    public void onImageSelected(Uri uri) {
+        imageViewNews.setImageURI(uri);
+        uriSelectedImage = uri;
+    }
+
+    public void putImageInStorage(String newsID) {
+        StorageReference newsStorageReference = storage.getReference(FOLDER_NAME).child(newsID);
+        if (uriSelectedImage != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            newsStorageReference.putFile(uriSelectedImage)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = 100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount();
+                            progressBar.setProgress((int) progress);
+                        }
+                    });
+        } else {
+            Toast.makeText(AdminNewsAddActivity.this, "Please click on photo to select image", Toast.LENGTH_LONG).show();
+            System.out.println("fuck");
+        }
     }
 }
