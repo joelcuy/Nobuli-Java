cfc865b4df5b4d26c30ae2cc88726a1cf3b2f91a
Joel Cheah <gjoelcheah@gmail.com>
2021-12-24 21:57:15 +0800

    user side news list done

--- app/src/main/java/com/example/nobulijava/activity/UserNewsListActivity.java
+++ app/src/main/java/com/example/nobulijava/activity/UserNewsListActivity.java
@@ -1,16 +1,61 @@
 package com.example.nobulijava.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.nobulijava.R;
+import com.example.nobulijava.adapters.NewsAdapter;
+import com.example.nobulijava.model.NewsObj;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class UserNewsListActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewNewsList;
+
+    private DatabaseReference mDatabase;
+
+    private ArrayList<NewsObj> newsObjArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_news_list);
+
+        getSupportActionBar().setTitle("News");
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        recyclerViewNewsList = findViewById(R.id.recyclerView_newsList);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabase.child("News").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                newsObjArrayList = new ArrayList<>();
+                for (DataSnapshot quizSnapshot: dataSnapshot.getChildren()) {
+                    NewsObj newsObj = quizSnapshot.getValue(NewsObj.class);
+                    newsObj.setNewsID(quizSnapshot.getKey());
+                    newsObjArrayList.add(newsObj);
+
+                }
+                NewsAdapter newsAdapter = new NewsAdapter(newsObjArrayList, UserNewsListActivity.this);
+                recyclerViewNewsList.setAdapter(newsAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
