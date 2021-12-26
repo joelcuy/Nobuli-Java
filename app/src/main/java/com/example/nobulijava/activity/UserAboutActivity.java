068e99efe47b8dc75c900c38983a75d5cad8a5b5
Joel Cheah <gjoelcheah@gmail.com>
2021-12-26 23:05:59 +0800

    user end, about get data from firebase

--- app/src/main/java/com/example/nobulijava/activity/UserAboutActivity.java
+++ app/src/main/java/com/example/nobulijava/activity/UserAboutActivity.java
@@ -1,12 +1,26 @@
 package com.example.nobulijava.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.nobulijava.R;
+import com.example.nobulijava.model.AboutCyberbullyObj;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ktx.Firebase;
 
 public class UserAboutActivity extends AppCompatActivity {
+
+    private TextView textViewAboutCyberbully;
+    private TextView textViewContact;
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,26 @@ public class UserAboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_about);
 
         getSupportActionBar().setTitle("About Cyberbullying");
+
+        findViewById();
+
+        mDatabase.child("About").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                AboutCyberbullyObj aboutCyberbullyObj = snapshot.getValue(AboutCyberbullyObj.class);
+                textViewAboutCyberbully.setText(aboutCyberbullyObj.getAboutCyberbully());
+                textViewContact.setText(aboutCyberbullyObj.getContact());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void findViewById() {
+        textViewAboutCyberbully = findViewById(R.id.textView_userAbout_aboutCyberbullying);
+        textViewContact = findViewById(R.id.textView_userAbout_contact);
     }
 }
